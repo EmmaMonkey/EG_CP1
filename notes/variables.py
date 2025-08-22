b2f93a95e9ab73f0e873250cd74eb4a49b561441
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-08-22 10:47:51 -0600

    Who are you project

---
+++ notes/variables.py
@@ -0,0 +1,10 @@
+# EG 2nd Variables Notes
+
+students_count = 29
+amount_water = "1oz"
+name = "Denzel"
+grade = 67
+print(grade)
+
+grade = 57
+print(grade)
--- practices/hello_world.py
+++ practices/hello_world.py
@@ -1,4 +1,4 @@
 #EG 2md Hello World 
 
 name = input("What's your name?")
-print("Hello,",name)
+print("Hello, LOOK WHAT YOU HAVE DONE MONKEY,",name)
