33724b1251e4fd95dc94ad1753aaa5fcba41574e
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-08-27 10:21:54 -0600

    Projects

--- projects/who_are_you.py
+++ projects/who_are_you.py
@@ -4,4 +4,4 @@ name=input("What's your name GURT? ")
 age=input("How old are you UNC? ")
 color=input("What's your favorite color buddy? ")
 
-print("Wow,",name,"i like,",color,"too, How old are you again?,",age,",!YOU ARE!,",age,"!GYATT! !DANGG YOUR OLD UNCC!")
+print("Wow,",name,",i like,",color,"too, How old are you again?,",age,",!YOU ARE!,",age,"!GYATT! !DANGG YOUR OLD UNCC!")
