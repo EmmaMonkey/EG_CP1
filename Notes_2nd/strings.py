e9c5b5af0da101fd441372900282b45c16b1f31e
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-09-03 10:51:10 -0600

    Strings notes

--- Notes_2nd/strings.py
+++ Notes_2nd/strings.py
@@ -1,3 +1,32 @@
 # EG 2nd Strings Notes
 
- 
+#Examples
+sentence = "The quick brown fox jumps over the lazy dog!"
+first_name = 'Xavier'
+month = 'September'
+food = "chocolate"
+last_name = "Guerra"
+
+# If I need to use ' or "
+name = "Balon D'Or"
+best = '"The Return of the King" is the best book ever!"'
+
+funny = "The dog jumped into the lake"
+
+# Length of strings
+length = len(month)
+print("September has", length, "letters")
+
+# Escape Character 
+print('\tWill said: "Don\'t die')
+
+#Concatenate
+full_name = first_name + " " + last_name 
+
+print(full_name)
+
+#Index
+
+
+#Slice
+print(last_name[2:1])
---
+++ practices/mad_lib.py
@@ -0,0 +1,31 @@
+#EG 2nd Mad Lib
+
+adjective = input ("Give me a cool adjective YOGURT!")
+
+animal =  input ("Give me an animal that you really really like." )
+
+verbing = input ("Give me a ver ending in -ing.")
+
+silly_object = input ("Give me the name of a really silly object.")
+
+type_of_dance = input ("Give me a type of dance Monkey!")
+
+body_Part = input ("Give me a part of the body that is not the elbow.")
+
+famous_person = input ("Give me the name of a celebrity or famous person, (can be fictional)")
+
+past_verb = input ("Give me a verb in past-tense Gurt!")
+
+food = input ("What's something ")
+
+emotion = input ("Whats an emotion you feel a lot?")
+
+place = input ("Give me a place Broski!")
+
+ridiculous_sound = input ("Give me the most ridiculous sound you can think of Gurt!")
+
+verbed = input("Give me a verb ending in -ED Jit!")
+
+exclamation = input("Give me an exclamation!")
+
+print("Eve was a(n),"+adjective+"")
