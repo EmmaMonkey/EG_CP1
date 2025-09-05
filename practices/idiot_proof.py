6f5dfb539c2ad57bd8ca818855e26de6714345a7
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-09-05 10:50:47 -0600

    Idiot proof

---
+++ practices/idiot_proof.py
@@ -0,0 +1,13 @@
+#EG 2nd Idiot proof
+
+full_name=input("What's your full name: ").strip().lower().capitalize().title()
+
+phone_number=input("Yo, Gurt, What's your phone number: ")
+
+GPA=input("what's your GPA currently: ")
+
+print(phone_number.isdigit())
+
+print(GPA.isdigit())
+
+print("Wow,"+full_name+",That's a really nice name, you know I rizzed you up right? I lowkey want you now, Actually no imma leak your phone number, it's,"+phone_number+",right? lemme check your GPA and ill tell you if its valid,"+GPA+", OH, FLIP NAH, YOU DUMB TWIN ION WANT YOU NO MORE.")
--- practices/mad_lib.py
+++ practices/mad_lib.py
@@ -16,7 +16,7 @@ famous_person = input ("Give me the name of a celebrity or famous person, (can b
 
 past_verb = input ("Give me a verb in past-tense Gurt!")
 
-food = input ("What's something ")
+food = input ("What's something that is edible")
 
 emotion = input ("Whats an emotion you feel a lot?")
 
@@ -28,4 +28,4 @@ verbed = input("Give me a verb ending in -ED Jit!")
 
 exclamation = input("Give me an exclamation!")
 
-print("Eve was a(n),"+adjective+", She was always happy tho. She always said her spirit animal was,"+animal+", Everyone was very annoyed by her and her spirit animal thing that they thought she was,"+adjective+",She also loved to,"+verbing+",and always carried a,"+silly_object+",This bothered Emma, and she had to betray him because she wanted to and there was no other choice, so she "+type_of_dance+",on him. Emma was full of it he was angry up to his."+body_Part+",He wanted to get revenge so he called up,"+famous_person+", Emma was so mad he,"+verbed+","+famous_person+",")
+print("Eve was a(n),"+adjective+", She was always happy tho. She always said her spirit animal was a,"+animal+", Everyone was very annoyed by her and her spirit animal thing that they thought she was,"+adjective+",She also loved to,"+verbing+",and always carried a,"+silly_object+",This bothered Emma, and she had to betray him because she wanted to and there was no other choice, so she "+type_of_dance+",on him. Emma was full of it he was angry up to his."+body_Part+",He wanted to get revenge so he called up,"+famous_person+", Emma was so mad he,"+past_verb+","+famous_person+",and he ate a,"+food+", He desperately was wanting revenge and,"+emotion+", Finally, he thought of the perfect plan. He was going to send Eve to,"+place+", He was so escited about his plan he,"+ridiculous_sound+", and he couldn't wait. He knew Eve would miss him and come back and apologize and become his elbow partner again. So, end of the story is that he didn't have enough money to send Eve to,"+place+", so he just,"+verbed+", and just cried and cried because his work was really hard and Eve didn't come back. He,"+exclamation+" and the End he was sad thats it.")
