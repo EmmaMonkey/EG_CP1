3e798ba6168bdebe3be2c7794766700c26495788
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-09-08 10:51:20 -0600

    basic calculator

---
+++ practices/basic_calculator.py
@@ -0,0 +1,7 @@
+# EG 2nd Basic Calculator
+
+print(" This is a calculator, addition is (+) ")
+number1 = int(input("give me a number\n"))
+number2 = input ("give me another number")
+
+print(number1*number2)
