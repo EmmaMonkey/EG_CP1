e93087760fef469a2dfcc7e934363946b1b83a0c
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-09-09 16:26:58 +0000

    Added print to test.py

--- Notes_1st/test.py
+++ Notes_1st/test.py
@@ -1,5 +1,5 @@
 print("Test")
 print("Update")
 print("Practice sync and homework")
+print("I added this on codespaces. That place where I can do homework.")
 #Print() <= Everything inside the paranthesis appears in the terminal
-
