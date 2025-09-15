3521f9fb5ac94f89673a22d12ba692f6c5f99fa1
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-09-15 10:48:49 -0600

    Crew shares fix

--- projects/crew_shares.py
+++ projects/crew_shares.py
@@ -3,7 +3,7 @@
 
 print("The crew earned a whole bunch of money on the last outing, but the captain didn't have time to divvy it all up before release everyone to port.He gave each member of the crew 500 dollars for the evening and then sat down with his first mate to properly divide the shares.")
 
-money_earned = float0(input("How much money was made in the outing.(number has to be a whole number without decimals): "))
+money_earned = float(input("How much money was made in the outing.(number has to be a whole number without decimals): "))
 crew_num = int(input("How many crew members were there not including the captain and the first mate."))
 
 equal_share = money_earned/(crew_num+10)
