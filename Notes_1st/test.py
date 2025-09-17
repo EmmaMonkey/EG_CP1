bba3411a47870e7c65eb336c34dfe901c6f6a0d7
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-09-17 10:45:55 -0600

    Got a new seat

--- Notes_1st/test.py
+++ Notes_1st/test.py
@@ -2,4 +2,5 @@ print("Test")
 print("Update")
 print("Practice sync and homework")
 print("I added this on codespaces. That place where I can do homework.")
+print("new seat 9/17")
 #Print() <= Everything inside the paranthesis appears in the terminal
