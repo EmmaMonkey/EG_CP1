954441634427634d24b614b41782424ea7c167b9
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-09-22 10:25:34 -0600

    User sign in

--- practices/user.signin.py
+++ practices/user.signin.py
@@ -1,2 +1,10 @@
 #EG 2nd User Sign In
 
+
+user = input("What's your username:")
+password = input("what's your password:")
+
+if user == "EmmaMonkey" and password == "Emmanuel0112":
+    print(" Welcome, Username and password are correct")
+else:
+    print("Invalid Username and/or password")
