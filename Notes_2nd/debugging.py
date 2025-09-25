1385ddd360d5030da6544910f1a7a06bf1df8b45
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-09-25 10:50:51 -0600

    Debugging

---
+++ Notes_2nd/debugging.py
@@ -0,0 +1,10 @@
+# EG 2nd Debugging Notes
+
+# Syntax Errors-Grammar Error
+#print("Ms. LaRose")
+# Fix by => reading where the problem is AND fixing it!
+
+# Logic Errors
+num = "10"
+num_two = "3"
+# Run-Time Errors
