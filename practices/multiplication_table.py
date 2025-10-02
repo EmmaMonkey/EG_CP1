340c12a6e485fb32c6b5d2e4838445d7501027f1
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-10-02 10:44:25 -0600

    multiplication table

--- practices/multiplication_table.py
+++ practices/multiplication_table.py
@@ -2,6 +2,7 @@
 
 nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
+
 for num in nums:
-    print(num)
-    mult = num*nums
+    num*1, num*2, num*3, num*4, num*5, num*6, num*7, num*8, num*9, num*10, num*11, num*12, num*13, num*14, num*15
+    print(num*1, num*2, num*3, num*4, num*5, num*6, num*7, num*8, num*9, num*10, num*11, num*12, num*13, num*14, num*15)
