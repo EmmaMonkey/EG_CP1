af6ae6dd7a32350064732cc9d011ba6eba6ba811
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-10-06 10:28:40 -0600

    Rock Paper scissors

--- practices/rock_paper_scissors.py
+++ practices/rock_paper_scissors.py
@@ -1,7 +1,9 @@
 # EG 2nd  Rock Paper Scissors
-
+import random
+import time
 
 # Rock
+print("Welcome to Rock, Paper, Scissors! ")
 print("""
     _______
 ---'   ____)
@@ -30,3 +32,27 @@ print("""
       (____)
 ---.__(___)
 """)
+
+choices = ["rock", "paper", "scissors"]
+
+while True:
+    user = input("Rock, paper, or scissors? (or type 'quit' to stop): ").lower()
+
+    if user == "quit":
+        print("Bye! Thanks for playing.")
+        break
+    if user not in choices:
+        print("That's not a valid choice. Try again.")
+        continue
+
+    computer = random.choice(choices)
+    print(f"Computer chose {computer}.")
+
+    if user == computer:
+        print("It's a tie!\n")
+    elif (user == "rock" and computer == "scissors") or \
+         (user == "scissors" and computer == "paper") or \
+         (user == "paper" and computer == "rock"):
+        print("You win!\n")
+    else:
+        print("Computer wins!\n")
