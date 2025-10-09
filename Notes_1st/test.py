091883e890b15b3cee5261e6c1477ed9a1a8c5d9
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-10-09 10:50:10 -0600

    tic tac toe

--- Notes_1st/test.py
+++ Notes_1st/test.py
@@ -4,3 +4,17 @@ print("Practice sync and homework")
 print("I added this on codespaces. That place where I can do homework.")
 print("new seat 9/17")
 #Print() <= Everything inside the paranthesis appears in the terminal
+
+
+#Shades on, I'm Boul Wit Da Glasses
+#Bro say er 'cause he a savage
+#So many dead opps, so many ashes (brrt)
+#You ain't catch that, I can't pass this (come here)
+#Shooter stay strapped, I don't need mine
+#Bro put belt right to they behind
+#The way that switch brrt, I know he dyin' (oh my, oh my God)
+#6-7, I just bipped right on the highway (bip, bip)
+#Skrrt, uh (bip, bip, bip)
+#I just bipped right on the highway
+#Trackhawk, mh, sittin' in the driveway (skrrt)
+#Uh, pull up, doot-doot, doo-doo-doo
