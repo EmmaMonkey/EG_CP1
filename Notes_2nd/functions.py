b9a4602e8909e028267f81bd245354423c265623
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-10-20 10:54:01 -0600

    functions notes

---
+++ Notes_2nd/functions.py
@@ -0,0 +1,14 @@
+# EG 2nd Functions Notes
+
+num = 0
+def add(x,y):
+    return x+y
+
+while num < add(5,5):
+    print("Duck")
+    num +=1 
+print("Goose")
+print(f"Result is: {add(-654386878523,5237983)}")
+total = add(3874983274,34)
+print(add(add(3.14,.85), 10))
+add(42,7)
