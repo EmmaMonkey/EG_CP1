b30185f3fbe3f5b7e5b1b806808f6cefc68d0d7f
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-10-24 10:51:05 -0600

    combat program update

--- practices/combat.py
+++ practices/combat.py
@@ -1,1 +1,8 @@
 # EG 2nd Combat Program
+import random
+print("Welcome to training! First I need to know some things about you!")
+name = input("What is your name?: ")
+print(f"Welcome {name}")
+print("There are 3 fighter types: Fighter, Mage, Rogue:")
+fighter_class = input(f"What class of fighter are you {name}?: ")
+print(f"Thats great {name}, you are a {fighter_class}. Here are your stats! ")
