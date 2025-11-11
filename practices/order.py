d536fb0f00bc843b50664158895db48f09ddd528
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-11-11 10:51:26 -0700

    order

--- practices/order.py
+++ practices/order.py
@@ -1,1 +1,12 @@
 # EG 2nd Order Up!
+
+#Make the menu dictionary
+menu = {
+    "Main Dishes": {
+        "The Box Combo": 11.99,
+        "The 3 Finger Combo": 10.39,
+        "The Caniac Combo": 17.29,
+        "The Sandwich Combo": 10.79,
+        "The Kids Combo": 6.99,
+    }
+}
