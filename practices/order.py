3b58ab356e5dc54f8daad8d0fa0fab940ec7c373
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-11-12 10:49:22 -0700

    order

--- practices/order.py
+++ practices/order.py
@@ -8,5 +8,22 @@ menu = {
         "The Caniac Combo": 17.29,
         "The Sandwich Combo": 10.79,
         "The Kids Combo": 6.99,
+    },
+    "Side Dishes": {
+        "Chicken Fingers": 1.99,
+        "Crinkle-Cut Fries": 2.59,
+        "Cane's Sauce": 0.39,
+        "Texas Toast": 1.35,
+        "Coleslaw": 1.35,
+        "Sandwich": 8.49,
+    },
+    "Drink": {
+        "Lemonade": 2.79,
+        "Sweet Tea": 2.49,
+        "Coke": 2.49,
+        "Diet Coke": 2.49,
+        "Coke Zero": 2.49,
+        "Sprite": 2.49,
+        "Fanta": 2.49,
     }
 }
