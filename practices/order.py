2d5df1beaa83a9499b2781894fc9c14dabb87982
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-11-14 14:00:34 +0000

    finished version

--- practices/order.py
+++ practices/order.py
@@ -1,6 +1,5 @@
-# EG 2nd Order Up!
 
-#Make the menu dictionary
+# Menu dictionary
 menu = {
     "Main Dishes": {
         "The Box Combo": 11.99,
@@ -27,3 +26,27 @@ menu = {
         "Fanta": 2.49,
     }
 }
+
+print("Welcome to Order Up!")
+
+# Show menu
+print("\nMain Dishes:", menu["Main Dishes"])
+print("Side Dishes:", menu["Side Dishes"])
+print("Drinks:", menu["Drink"])
+
+# Ask for items
+main = input("\nEnter your main dish: ")
+drink = input("Enter your drink: ")
+side1 = input("Enter your first side dish: ")
+side2 = input("Enter your second side dish: ")
+
+# Calculate total
+total = menu["Main Dishes"][main] + menu["Drink"][drink] + menu["Side Dishes"][side1] + menu["Side Dishes"][side2]
+
+# Print order summary
+print("\nYour Order:")
+print("Main Dish:", main)
+print("Drink:", drink)
+print("Side Dish 1:", side1)
+print("Side Dish 2:", side2)
+print("Total Cost: $", round(total, 2))
