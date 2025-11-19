30b86f8564f4ac671bd10f3a4b5a85c4a4b49ea3
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-11-19 10:50:58 -0700

    squares practice

---
+++ practices/squares.py
@@ -0,0 +1,12 @@
+#EG 2nd Squared Numbers
+# List the numbers
+numbers = [641,94,5619,135,4,6501,68,7,123,54,6,4,67,42,2,23,4,35,465,354,24,1,4323,634,543,636,546,42,43235,]
+
+# Main Code
+def square(number):
+    return (number ** 2)
+
+squared = list(map(lambda num : (num**2), numbers)) 
+
+for i, num in enumerate(numbers):
+    print(f"{num} squared is {squared[i]}")
