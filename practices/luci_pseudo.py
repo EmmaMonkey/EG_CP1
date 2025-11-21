8219ed1095153d37786431ae888503c00b086f79
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-11-21 10:51:57 -0700

    pseudo

---
+++ practices/luci_pseudo.py
@@ -0,0 +1,33 @@
+#Initials,period,fractionals calculators
+#EG, 2nd, Fractionals Calculators
+
+#user input
+#ask the user to enter a number
+input("enter a number")
+inpiut = number
+number = str(())
+#make sure it is a number str() or int(),else print("invalid, please enter a number")
+'use an if statment'
+if number == int():
+    print()
+else:
+    print("invalid, please enter a number")
+#define the calculation equation
+def multiply(number):
+    answer = (number * number)-1
+
+#for product in factonal
+#write do the equation # x #-1 x #-2 x till you have 1 left and times it all
+
+#while #
+    #if # True 
+    #do the equation
+    #else
+    #print please enter # again
+#print answer to equation
+#ask the user if they want to do the equation again 
+#write done to end loop
+#if yes do it again
+#else
+#print: thank you for using our caluclator
+#return
