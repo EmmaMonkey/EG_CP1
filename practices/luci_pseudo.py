5a1dad4cf5987a89acc25adcd19a0937243f9a67
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-11-24 10:52:10 -0700

    factorial

--- practices/luci_pseudo.py
+++ practices/luci_pseudo.py
@@ -1,24 +1,20 @@
 #Initials,period,fractionals calculators
 #EG, 2nd, Fractionals Calculators
+print("Welcome to the Factorial Calculator")
 
 #user input
 #ask the user to enter a number
-input("enter a number")
-inpiut = number
-number = str(())
-#make sure it is a number str() or int(),else print("invalid, please enter a number")
-'use an if statment'
-if number == int():
-    print()
-else:
-    print("invalid, please enter a number")
-#define the calculation equation
-def multiply(number):
-    answer = (number * number)-1
+while True:
+    original = input("Enter a number to calculate its factorial (or type 'exit' to exit): ")
+    
+    if original.lower() == "done":
+        break
 
+    if not 
+#make sure it is a number str() or int(),else print("invalid, please enter a number")
+#define the calculation equation
 #for product in factonal
 #write do the equation # x #-1 x #-2 x till you have 1 left and times it all
-
 #while #
     #if # True 
     #do the equation
