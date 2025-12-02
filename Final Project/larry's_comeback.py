1cc305cd65871e71305f276dde483c40a329ef56
EmmaMonkey <emmanuel.guerra@ucas-edu.net>
2025-12-02 10:51:09 -0700

    larry

---
+++ Final Project/larry's_comeback.py
@@ -0,0 +1,10 @@
+#EG, 2nd Period, Larry's Comeback
+
+#import random
+
+#Welcome the User to the game and introduce the story of the game.
+
+#Make Larry start Goblin Stadium 
+
+#Make Goblin Stadium a Function
+
